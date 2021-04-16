d66442129a1689be28525900fc7c70fe4e3713c4
googol88 <ronaldfleung@gmail.com>
2021-04-15 20:04:27 -0400

    🐛 fix site responsiveness

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       </Head>
       <Text h1 size="2em" style={{textAlign:"center"}}>Repl Talk Badge Generator</Text>
       <Grid.Container>
-        <Grid sm={24} md style={{flexDirection: "column"}}>
+        <Grid xs={24} sm={24} md={16} style={{display: "block"}}>
           <Text style={{display: 'inline-block', width: '8em', textAlign: 'right', marginRight: '1em'}}>Repl Talk URL</Text>
           <Input placeholder="http://replit.com/talk/share/HelloWorld/123456" width="24em"/>
           <Spacer y={.5} />
@@ -43,8 +43,9 @@ export default function Home() {
 
           <Text style={{display: 'inline-block', width: '8em', textAlign: 'right', marginRight: '1em'}}>Alternate Text 2</Text>
           <Input placeholder="Repl Talk"/>
+          <Spacer y={2} />
         </Grid>
-        <Grid sm={24} md>
+        <Grid xs={24} sm={24} md={8}>
           <Image
             src="https://replit-badge.vercel.app/api?id=123456"
           />

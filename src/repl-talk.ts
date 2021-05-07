f7f50809e4fc95bf2b68407c386dd1f091eecb95
googol88 <ronaldfleung@gmail.com>
2021-05-07 19:19:50 -0400

    ⬆️  upgrade replapi

--- src/repl-talk.ts
+++ src/repl-talk.ts
@@ -1,15 +1,10 @@
 import ReplAPI from 'replapi-it'
 const replapi = ReplAPI();
 
-/*async function myTestFunction() {
-  const info = await myTestClass.userGraphQLDataFull();
-  console.log(info);
-}
-myTestFunction();*/
+
 export const getNumberOfUpvotes = async (id: number) => {
   const post = new replapi.Post(id);
-  const info = await post.postDataAbridged();
-  //console.log("Testing");
+  const info = await post.postDataFull();  
   
   console.log(info);
   return info.voteCount;

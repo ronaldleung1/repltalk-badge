037b81b87f2ea75daa7824a5b7df08263fd10ffd
googol88 <ronaldfleung@gmail.com>
2021-05-07 20:34:03 -0400

    🐛 use conditional exports from replapi package

--- src/repl-talk.ts
+++ src/repl-talk.ts
@@ -1,11 +1,10 @@
-const ReplAPI = require('replapi-it');
+const ReplAPI = require('replapi-it').default;
 const replapi = ReplAPI();
 
 
 export const getNumberOfUpvotes = async (id: number) => {
   const post = new replapi.Post(id);
   const info = await post.postDataFull();  
-  
   console.log(info);
   return info.voteCount;
 };

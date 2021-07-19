9754f13648d73ebf848fc54c0a0774b5c41303a1
googol88 <ronaldfleung@gmail.com>
2021-07-18 20:41:56 -0400

    🎨 Use promises instead

--- src/repl-talk.ts
+++ src/repl-talk.ts
@@ -3,15 +3,17 @@ import cheerio from "cheerio";
 
 export const getNumberOfUpvotes = async (id: string) => {
   let url = `https://replit.com/talk/share/${id}`;
-  console.log("Test");
-  axios.get('https://replit.com/talk/share/Replit-Logo-Made-out-of-Repliters/142833')
-    .then(async response => {
-      console.log(response.status);
-      if(response.status === 200) {
-        const html = await response.data;
-        const $ = cheerio.load(html);
-        return parseInt($("jsx-1975752611.vote-control-heavy").children().next().text());
-      }
-    }, (error) => console.log(error));
-  return 0;
+  return new Promise((resolve, reject) => {
+    axios.get('https://replit.com/talk/share/Replit-Logo-Made-out-of-Repliters/142833')
+      .then(async response => {
+        console.log(response.status);
+        if(response.status === 200) {
+          const html = await response.data;
+          const $ = cheerio.load(html);
+          const upvotes = $("jsx-1975752611.vote-control-heavy").children().last().text();
+          console.log("Upvotes: " + upvotes[0]);
+          resolve(parseInt(upvotes));
+        }
+      }, (error) => reject(console.log(error)));
+  });
 };

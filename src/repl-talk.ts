89fef4c83cc54321519905bb3d36e7c20e563a96
googol88 <ronaldfleung@gmail.com>
2021-11-17 18:49:34 -0500

    🐛 fix: add headers to get request

--- src/repl-talk.ts
+++ src/repl-talk.ts
@@ -4,7 +4,11 @@ import cheerio from "cheerio";
 export const getNumberOfUpvotes = async (id: number) => {
   let url = `https://replit.com/talk/share/post/${id}`;
   return new Promise((resolve, reject) => {
-    axios.get(url)
+    axios.get(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
+      }
+    })
       .then(async response => {
         if(response.status === 200) {
           const html = await response.data;
